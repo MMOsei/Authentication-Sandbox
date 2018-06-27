a57db32a7bdbfbca079f1d25a29d37611806626c
kenl94 <KLy@spartaglobal.com>
2018-06-27 12:00:22 +0100

    Generated devise controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @user = User.find(params[:user_id])
   end
 
   # GET /projects/new
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   default_url_options :host => "http://localhost:3000"
 
-  devise_for :users
-  # get 'home/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   root 'static#home'
+
+  # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
